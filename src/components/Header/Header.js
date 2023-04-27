60d9ab1e0fbf28ccef363105dd59cf3768ec150d
prejin2310 <prejinproffical@gmail.com>
2023-04-27 21:42:55 +0530

    create hero section

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Header.module.css';
+import resume from '../../assets/resume.svg'
+
+const Header = () => {
+  return (
+    <div className={styles.container}>
+        <div className={styles.left}>
+            <p className={styles.heading}>A <span>Resume</span> that stands out!</p>
+            <p className={styles.heading}>Make your own resume <br/><span> Its free..</span> </p>
+        </div>
+        <div className={styles.right}>
+            <img src={resume} alt='Resume Job'/>
+        </div>
+    </div>
+  )
+}
+
+export default Header
