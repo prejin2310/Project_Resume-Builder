35e9184e8b931678b39449168d3cac31483ae795
prejin2310 <prejinproffical@gmail.com>
2023-04-29 14:20:08 +0530

    resume build section finished

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,77 +1,106 @@
-import React, { useState } from 'react'
-import styles from './Body.module.css';
-import { Download } from 'react-feather';
-import Editor from '../Editor/Editor';
+import React, { useRef, useState } from "react";
+import ReactToPrint from "react-to-print";
+import { ArrowDown } from "react-feather";
 
-const Body = () => {
-    const colors=['#6DB56D','#F4613E','#5B7FC5','#5534A5','#8B1540','#000000']
-    const sections ={ 
-        basicInfo: "Basic Info",
-        workExp: "Work Experience",
-        project: "Projects",
-        education: "Education",
-        achievements: "Achievements",
-        summary: "Summary",
-        other: "Other",
-    };
+import Editor from "../Editor/Editor";
+import Resume from "../Resume/Resume";
 
-    const [resumeInformation, setResumeInformation] = useState({
-        [sections.basicInfo]: {
-          id: sections.basicInfo,
-          sectionTitle: sections.basicInfo,
-          detail: {},
-        },
-        [sections.workExp]: {
-          id: sections.workExp,
-          sectionTitle: sections.workExp,
-          details: [{title:"Work",
-                     
-        }],
-        },
-        [sections.project]: {
-          id: sections.project,
-          sectionTitle: sections.project,
-          details: [],
-        },
-        [sections.education]: {
-          id: sections.education,
-          sectionTitle: sections.education,
-          details: [],
-        },
-        [sections.achievement]: {
-          id: sections.achievement,
-          sectionTitle: sections.achievement,
-          points: [],
-        },
-        [sections.summary]: {
-          id: sections.summary,
-          sectionTitle: sections.summary,
-          detail: "",
-        },
-        [sections.other]: {
-          id: sections.other,
-          sectionTitle: sections.other,
-          detail: "",
-        },
-      });
+import styles from "./Body.module.css";
+
+function Body() {
+  const colors = ["#239ce2", "#48bb78", "#0bc5ea", "#a0aec0", "#ed8936"];
+  const sections = {
+    basicInfo: "Basic Info",
+    workExp: "Work Experience",
+    project: "Projects",
+    education: "Education",
+    achievement: "Achievements",
+    summary: "Summary",
+    other: "Other",
+  };
+  const resumeRef = useRef();
+
+  const [activeColor, setActiveColor] = useState(colors[0]);
+  const [resumeInformation, setResumeInformation] = useState({
+    [sections.basicInfo]: {
+      id: sections.basicInfo,
+      sectionTitle: sections.basicInfo,
+      detail: {},
+    },
+    [sections.workExp]: {
+      id: sections.workExp,
+      sectionTitle: sections.workExp,
+      details: [],
+    },
+    [sections.project]: {
+      id: sections.project,
+      sectionTitle: sections.project,
+      details: [],
+    },
+    [sections.education]: {
+      id: sections.education,
+      sectionTitle: sections.education,
+      details: [],
+    },
+    [sections.achievement]: {
+      id: sections.achievement,
+      sectionTitle: sections.achievement,
+      points: [],
+    },
+    [sections.summary]: {
+      id: sections.summary,
+      sectionTitle: sections.summary,
+      detail: "",
+    },
+    [sections.other]: {
+      id: sections.other,
+      sectionTitle: sections.other,
+      detail: "",
+    },
+  });
 
   return (
     <div className={styles.container}>
-        <p className={styles.heading}>Resume Builder</p>
-        <div className={styles.toolbar}>
-            <div className={styles.colors}>
-                {
-                    colors.map(item=>
-                        <span key={item} style={{backgroundColor:item}}className={styles.color}/>
-                )}
-            </div>
-            <button>Download <Download/></button>
+      <p className={styles.heading}>Resume Builder</p>
+      <div className={styles.toolbar}>
+        <div className={styles.colors}>
+          {colors.map((item) => (
+            <span
+              key={item}
+              style={{ backgroundColor: item }}
+              className={`${styles.color} ${
+                activeColor === item ? styles.active : ""
+              }`}
+              onClick={() => setActiveColor(item)}
+            />
+          ))}
         </div>
-        <div className={styles.main}>
-            <Editor sections={sections} information={resumeInformation} setInformation={setResumeInformation}/>
-        </div>
+        <ReactToPrint
+          trigger={() => {
+            return (
+              <button>
+                Download <ArrowDown />
+              </button>
+            );
+          }}
+          content={() => resumeRef.current}
+        />
+      </div>
+      <div className={styles.main}>
+        <Editor
+          sections={sections}
+          information={resumeInformation}
+          setInformation={setResumeInformation}
+        />
+        <Resume
+          ref={resumeRef}
+          sections={sections}
+          information={resumeInformation}
+          activeColor={activeColor}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Body
+export default Body;
